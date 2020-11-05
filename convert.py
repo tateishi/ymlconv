641d3494028e547a23a761340f251f726510f686
TATEISHI Tadatoshi <ishio39@gmail.com>
2020-11-06 01:49:21 +0900

    入出力ファイルの設定変更
    入出力のファイル名指定方法変更
    ファイルのエンコード指定追加

--- convert.py
+++ convert.py
@@ -2,13 +2,17 @@ import yaml
 from jinja2 import Template, Environment, FileSystemLoader
 
 def main(infile, outfile, tempfile='vcard.tmpl'):
-    with open(infile) as file:
+    with open(infile, encoding='utf-8') as file:
         yml = yaml.safe_load(file) 
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template(tempfile)
     result = template.render(yml)
-    with open(outfile, 'w') as out:
+    with open(outfile, 'w', encoding='utf-8') as out:
         print(result, file=out)
 
 if __name__ == '__main__':
-    main('family.yml', 'result.vcf')
+    import sys
+
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+    main(infile, outfile)
